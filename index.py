999549a64c3c9296989402d7a257927e797220e9
Elton Lau <eltondlau@gmail.com>
2016-08-08 10:07:57 +0800

    new png jp

--- index.py
+++ index.py
@@ -834,6 +834,7 @@ idList[1166] = {'id':1305, 'name-jp':'バカラ'}
 idList[1167] = {'id':1306, 'name-jp':'バカラ', 'title-jp':'黄金船のコンシェルジュ', 'skill':1305}
 idList[1168] = {'id':1307, 'name-jp':'ダイス', 'skill':8183}
 idList[1170] = {'id':1311, 'name-jp':'タナカさん', 'skill':8184}
+idList[1178] = {'id':522, 'name-jp':'オーズ'}
 idList[1184] = {'id':1316, 'name-jp':'双拳ガントレットのキングデュー', 'skill':1315}
 idList[1185] = {'id':1317, 'name-jp':'ハルタ'}
 idList[1187] = {'id':1319, 'name-jp':'アトモス'}
