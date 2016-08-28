78a7793c1e243662664e62566d9c9ce895011438
Elton Lau <eltondlau@gmail.com>
2016-08-28 13:05:18 +0800

    missing_index by lang

--- missing_index.sh
+++ missing_index.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-for f in $(find png/ -name character*t1* | awk -F/ '{print $4}' | sort | uniq | grep -Eo "_\d+_" | awk -F_ '{print $2}'); do
+for f in $(find png/$1/ -name character*t1* | awk -F/ '{print $4}' | sort | uniq | grep -Eo "_\d+_" | awk -F_ '{print $2}'); do
 	f=$((10#$f))
 	if [ $f -gt 2000 ]; then
 		continue;
 	fi
-	dummy=$(grep "'id':"$f"" "index.py")
+	dummy=$(grep "'id':"$f"" "index.py" | grep "$1")
 	if [ "$?" != 0 ]; then
-		find png/ -name *$f*
+		find "png/$1" -name *$f*
 	fi
 done
 
