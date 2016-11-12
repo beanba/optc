5a16927353ea116ed5ed1934bef8770c4b3ab8d0
Elton Lau <eltondlau@gmail.com>
2016-11-12 17:46:38 +0800

    remove -n1 from xargs

--- optc.sh
+++ optc.sh
@@ -6,12 +6,12 @@ ID=$2
 SUBDIR=$3
 
 if [ -n "$c1" ]; then
-  adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*c1.png | tr '\r' ' ' | xargs -n1 -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
+  adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*c1.png | tr '\r' ' ' | xargs -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
   exit 0
 fi
 
 if [ -n "$t1" ]; then
-  adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*t1.png | tr '\r' ' ' | xargs -n1 -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
+  adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*t1.png | tr '\r' ' ' | xargs -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
   exit 0
 fi
 
