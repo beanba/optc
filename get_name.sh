401c9b2829e6de2f17930f418bd27d76682ba99d
Elton Lau <eltondlau@gmail.com>
2016-11-14 14:33:15 +0800

    separate title in case with $3

--- get_name.sh
+++ get_name.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 for i in $(seq $1 $2); do
-  name=$(curl -s 'http://onepiece-treasurecruise.com/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
-  echo $i" 'name-jp':'"$name"',"
+  full=$(curl -s 'http://onepiece-treasurecruise.com/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
+  name=$(echo $full | awk -F' ' '{print $1}')
+  title=$(echo $full | awk -F' ' '{print $2}')
+  if [ -z "$3" ]; then
+    echo $i" 'name-jp':'"$full"',"
+  else
+    echo $i" 'name-jp':'"$name"',"" 'title-jp':'"$title"',"
+  fi
 done
 
 exit 0
