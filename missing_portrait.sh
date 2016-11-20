258a24ed7512343659babc32d623f1896a6c1da9
Elton Lau <eltondlau@gmail.com>
2016-11-20 15:23:22 +0800

    enhance missing_portrait.sh scanning

--- missing_portrait.sh
+++ missing_portrait.sh
@@ -1,3 +1,16 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
- find -E png/$1  -regex ".*character_[5|6][0-9]{3}_t1\.png" | sed 's/_5/_0/' | sed 's/_6/_1/' | sed 's/_t1/_c1/' | xargs -n1 ls
+a=$(find -E png/$1 -regex ".*character_[0|1][0-9]{3}_t1\.png" | sed 's/_t1/_c1/')
+b=$(find -E png/$1 -regex ".*character_[5|6][0-9]{3}_t1\.png" | sed 's/_5/_0/' | sed 's/_6/_1/' | sed 's/_t1/_c1/')
+
+for f in $(echo $a $b | xargs -n1 | sort | uniq); do
+  if [ ! -f $f ]; then
+  	thumbnail=$(echo $f | sed 's/_c1/_t1/')
+    if [ -f $thumbnail ]; then
+      echo $thumbnail
+    else
+      skill=$(echo $f | sed 's/_0/_5/' | sed 's/_1/_6/' | sed 's/_c1/_t1/')
+      echo $skill
+    fi
+  fi
+done
