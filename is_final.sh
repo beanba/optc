5d34af3952943f6b505c9de986bffb0b3d0441a2
Elton Lau <eltondlau@gmail.com>
2016-12-06 22:06:42 +0800

    get final state by no.

---
+++ is_final.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+id=$(grep -B7 "no\": $2," index.json | grep '"id":' | awk '{print $2}' | awk -F',' '{print $1}')
+sid=$(grep -A7 "no\": $2," index.json | grep '"sid":' | awk '{print $2}' | awk -F',' '{print $1}')
+
+padid=`printf %04d $id`
+padsid=`printf %04d $sid`
+
+find png/$1 -name "*character_"$padid"_*"
+find png/$1 -name "*_"$padsid"*" | grep skill
