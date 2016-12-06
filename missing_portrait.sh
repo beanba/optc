0e4a16341a02358f525bbb4751e6a16372d2bdd6
Elton Lau <eltondlau@gmail.com>
2016-12-06 09:59:59 +0800

    print missing portrait with no.

--- missing_portrait.sh
+++ missing_portrait.sh
@@ -7,10 +7,14 @@ for f in $(echo $a $b | xargs -n1 | sort | uniq); do
   if [ ! -f $f ]; then
   	thumbnail=$(echo $f | sed 's/_c1/_t1/')
     if [ -f $thumbnail ]; then
-      echo $thumbnail
+      id=$(echo $f | awk -F'/' '{print $3}' |  awk -F'_' '{print $2}' | sed -E 's/^0+//')
+      no=$(grep -A7 "\"id\": $id," index.json  | grep '"no":' | awk '{print $2}' | awk -F',' '{print $1}')
+      echo $thumbnail $no
     else
       skill=$(echo $f | sed 's/_0/_5/' | sed 's/_1/_6/' | sed 's/_c1/_t1/')
-      echo $skill
+      id=$(echo $f | awk -F'/' '{print $3}' |  awk -F'_' '{print $2}' | sed -E 's/^0+//')
+      no=$(grep -A7 "\"id\": $id," index.json  | grep '"no":' | awk '{print $2}' | awk -F',' '{print $1}')
+      echo $skill $no
     fi
   fi
 done
