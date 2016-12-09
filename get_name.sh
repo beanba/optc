b4dc145d55621073c31252d38ad3045530f01020
Elton Lau <eltondlau@gmail.com>
2016-12-09 16:50:52 +0800

    get_name.sh print full string for jp and us

--- get_name.sh
+++ get_name.sh
@@ -2,13 +2,10 @@
 
 for i in $(seq $1 $2); do
   full=$(curl -s 'http://onepiece-treasurecruise.com/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
-  name=$(echo $full | awk -F' ' '{print $1}')
-  title=$(echo $full | awk -F' ' '{print $2}')
-  if [ -z "$3" ]; then
-    echo $i" 'name-jp':'"$full"',"
-  else
-    echo $i" 'name-jp':'"$name"',"" 'title-jp':'"$title"',"
-  fi
+  fullus=$(curl -s 'http://onepiece-treasurecruise.com/en/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
+
+  echo "$i: jp: $full"
+  echo "$i: us: $fullus"
 done
 
 exit 0
