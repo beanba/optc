454077ff2c7092d905f7a8e06c1fee3799557d47
Elton Lau <eltondlau@gmail.com>
2016-12-09 09:03:57 +0800

    print time in is_final.sh output

--- is_final.sh
+++ is_final.sh
@@ -6,5 +6,5 @@ sid=$(grep -A7 "no\": $2," index.json | grep '"sid":' | awk '{print $2}' | awk -
 padid=`printf %04d $id`
 padsid=`printf %04d $sid`
 
-find png/$1 -name "*character_"$padid"_*"
-find png/$1 -name "*_"$padsid"*" | grep skill
+find png/$1 -name "*character_"$padid"_*" | xargs ls -l
+find png/$1 -name "*_"$padsid"*" | grep skill | xargs ls -l
