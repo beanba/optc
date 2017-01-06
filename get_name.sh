b8adf96f5d644e509d38cae2403e17dc18b777f8
Elton Lau <eltondlau@gmail.com>
2017-01-06 23:12:17 +0800

    get_name.sh get tw names

--- get_name.sh
+++ get_name.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-for i in $(seq $1 $2); do
+start=$1
+if [ -n "$2" ]; then
+  end=$2
+else
+  end=$1
+fi
+
+for i in $(seq $start $end); do
   full=$(curl -s 'http://onepiece-treasurecruise.com/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
+  fulltw=$(curl -s 'http://line-optc.com/tw/c-'$i'/' | grep -A1 'class="single">' | grep h1 | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
   fullus=$(curl -s 'http://onepiece-treasurecruise.com/en/c-'$i'/' | grep 'monster_name' | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')
 
   echo "$i: jp: $full"
+  echo "$i: tw: $fulltw"
   echo "$i: us: $fullus"
 done
 
