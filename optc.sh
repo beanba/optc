484b4431538e178b37f965abf0725d0bb3d6b1da
Elton Lau <eltondlau@gmail.com>
2017-02-01 18:12:03 +0800

    new png jp

--- optc.sh
+++ optc.sh
@@ -10,6 +10,11 @@ if [ -n "$c1" ]; then
   exit 0
 fi
 
+if [ -n "$c2" ]; then
+  adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*c2.png | tr '\r' ' ' | xargs -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
+  exit 0
+fi
+
 if [ -n "$t1" ]; then
   adb shell ls /data/data/$PKG/files/Cache/GNPCACHE/*/*t1.png | tr '\r' ' ' | xargs -J % adb pull % $BASEDIR'/png/'$SUBDIR'/'
   exit 0
